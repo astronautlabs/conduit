7cc3af8816143a9e8b3010acf1da12da8944cafe
William Lahti <wilahti@gmail.com>
2023-02-18 22:32:35 -0800

    more docs

--- src/remote-ref.ts
+++ src/remote-ref.ts
@@ -6,6 +6,11 @@ export interface RemoteRef {
      * When encoding, this must always be a fresh GUID.
      */
     Rid?: string;
+
+    /**
+     * The side of the connection this reference applies to, from the perspective of 
+     * the sender. 'L' means local to the sender, 'R' means remote to the sender.
+     */
     S: 'L' | 'R'
 }
 
