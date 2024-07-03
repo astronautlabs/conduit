bb9f427a26df2fca9aaada6b54304025cfd4292c
William Lahti <wilahti@gmail.com>
2024-07-03 01:21:00 -0700

    remove setup comments

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,2 @@
-// For some reason sometimes this setup is not running (yielding errors for Zone being
-// undefined). This is here to help in diagnosing the issue.
-
 import "reflect-metadata";
 import "zone.js";
